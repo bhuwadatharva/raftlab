6d1d6b85b06c0a4d025f6c01c274cad1196f3e75
Atharva Nandkumar Bhuwad <bhuwad.atharva@gmail.com>
2024-09-26 23:47:44 +0530

    Update dbConnection.js

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
     mongoose.connect(process.env.MONGO_URL,{
-        dbName: "TO_DO_LIST"   //it is a general syntax to check the database which we are using is connected successfully
+        dbName: "TO_DO_LIST_DEPLOYED"   //it is a general syntax to check the database which we are using is connected successfully
     }).then(()=>{
         console.log("Connected to database!")
     }).catch(err=>{
