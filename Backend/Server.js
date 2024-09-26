0db8cd25a43c9cb12e1826adc88bb8590010bf33
Atharva Nandkumar Bhuwad <bhuwad.atharva@gmail.com>
2024-09-26 22:16:27 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
 app.listen(process.env.PORT, () =>{
-    console.log(`Server listening on port ${process.env.PORT}`); // THIS WAS USE TO CHECK WHETHER THE PATH GIVEN IN APP.JS OF CONFIG IS CORRECT OR NOT AND IT IS IN WORKING 
+    console.log(`Server listening on port ${process.env.PORT}`); 
 });
