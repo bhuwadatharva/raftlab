f1ea9825a54490734818002a6f692fe05ff1635e
Atharva Nandkumar Bhuwad <bhuwad.atharva@gmail.com>
2024-09-27 15:15:43 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -7,16 +7,23 @@ import { dbConnection } from "./database/dbConnection.js";
 
 const app = express();
 
+const allowedOrigins = ['https://raftlab-1txb.vercel.app/', 'https://raftlab.onrender.com/']; // Add your deployed frontend URL here
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 
 
 config({ path: "./config/config.env" });
 
-app.use(cors({
-  origin: [process.env.FRONTEND_URL],
-  methods: ["GET", "POST", "DELETE", "PUT"],
-  credentials: true,
-}));
+
 
 
 app.use(express.json());
